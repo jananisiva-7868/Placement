53eb497996585644946544c58bdaf9f1cfcd2a23
jananisiva-7868 <jananisiva47@gmail.com>
2025-06-09 21:42:14 +0530

    Add files via upload

---
+++ task2.java
@@ -0,0 +1,35 @@
+
+//TASK 2:REVERSE THE STRING IN THE GIVEN ARRAY USING SPLIT
+//package Placement;
+//
+//public class task2{
+//	public static void main(String[]args) {
+//		String str="Java Programming Language";
+//		String str1[]=str.split(" ");
+//		System.out.print(str1[2] + str1[1] +str1[0] );
+//	}
+//}
+
+
+
+//TASK 2:VOWELS AND CONSTANTS COUNT
+//package Placement;
+//public class task2{
+//	public static void main(String[]args) {
+//		int vowels=0;
+//		int constant=0;
+//		String str="apple";
+//		for(int i=0;i<str.length();i++) {
+//		char ch = str.charAt(i);
+//		if(ch=='a' || ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+//	  vowels++;
+//		}
+//		else {
+//			constant++;
+//		}
+//		}
+//		System.out.println("vowels = "+vowels);
+//		System.out.println("constants = "+constant);
+//		
+//	}
+//}
